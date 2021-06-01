7376e1bc221cf82b2cc09e4675d4c95e0abf33bc
guoxifeng1990 <76815589+guoxifeng1990@users.noreply.github.com>
2021-06-01 18:26:03 +0200

    Update graphreader.h
    correction

--- valhalla/baldr/graphreader.h
+++ valhalla/baldr/graphreader.h
@@ -497,7 +497,7 @@ public:
    * @param tile    reference to a pointer to a const tile
    * @return        returns GraphId of begin node of the edge (empty if couldn't find)
    */
-  GraphId GetBeginNodeId(const DirectedEdge* edge, graph_tile_ptr& tile) {
+  GraphId GetBeginNodeId(const DirectedEdge* edge, GraphTile*& tile) {
     // grab the node
     if (!GetGraphTile(edge->endnode(), tile))
       return {};
